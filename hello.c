32d66dfdd8dfe3cddb60bd599e52c60094285400
叶伟祥 <2390542198@qq.com>
2026-05-01 00:28:02 +0800

    第二次提交：增加input键盘输入功能

--- hello.c
+++ hello.c
@@ -1,5 +1,15 @@
-#include<stdio.h> 
+#include <stdio.h>
+
 int main() {
-    printf("Hello, World!\n");
+    char content[100];  // 用来存放输入的内容
+    
+    // 提示输入
+    printf("请输入内容：");
+    // 读取键盘输入
+    gets(content);
+    
+    // 输出输入的内容
+    printf("你输入的是：%s\n", content);
+    
     return 0;
 }
